c34ea98ecde4d7c8e6a92f1e53167fe6efc41be3
Kumar Mayank <103871423+Iamkrmayank@users.noreply.github.com>
2024-09-13 12:24:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title('Generate your webstories:😀')
 tab1, tab2 = st.tabs(["Master Template Generator", "Story Generator"])
 
 # Tab 1: Master Template Generator
-with tab1:
+'''with tab1:
     st.header('Master Template Generator')
     
     # File upload for Excel and HTML
@@ -38,6 +38,51 @@ with tab1:
                            mime='text/html')
 
         st.success("HTML content modified. Click the button above to download the modified file.")
+    else:
+        st.info("Please upload both an Excel file and an HTML file for the Master Template Generator.")'''
+# Tab 1: Master Template Generator
+with tab1:
+    st.header('Master Template Generator')
+    
+    # File upload for Excel and HTML
+    uploaded_excel_master = st.file_uploader("Upload the Excel file (for replacements)", type="xlsx", key="master_excel")
+    uploaded_html_master = st.file_uploader("Upload the HTML file", type="html", key="master_html")
+
+    # Proceed if both files are uploaded
+    if uploaded_excel_master and uploaded_html_master:
+        # Read the Excel file into a DataFrame
+        df_master = pd.read_excel(uploaded_excel_master, header=None)
+
+        # Read the uploaded HTML file
+        html_content_master = uploaded_html_master.read().decode('utf-8')
+
+        # Get the last row for placeholders
+        placeholder_row = df_master.iloc[-1]
+
+        # Loop through each row except the last one (which contains placeholders)
+        for row_index in range(len(df_master) - 1):
+            # Get the current row data (actual values)
+            row_data = df_master.iloc[row_index]
+
+            # Make a copy of the HTML content for each row
+            html_content_modified = html_content_master
+
+            # Perform replacements for each column
+            for col_index in range(len(df_master.columns)):
+                actual_value = str(row_data[col_index])      # Actual value from the current row
+                placeholder = str(placeholder_row[col_index])  # Placeholder from the last row
+                html_content_modified = html_content_modified.replace(actual_value, placeholder)
+
+            # Generate the filename using the first column of the current row
+            file_name = f"{str(row_data[0])}_template.html"
+
+            # Create a download button for each modified HTML
+            st.download_button(label=f"Download Modified HTML for {str(row_data[0])}", 
+                               data=html_content_modified, 
+                               file_name=file_name, 
+                               mime='text/html')
+
+        st.success("HTML content modified for all rows. Click the buttons above to download the modified files.")
     else:
         st.info("Please upload both an Excel file and an HTML file for the Master Template Generator.")
 
