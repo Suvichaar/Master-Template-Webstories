5ac39213d5b99ddc311af450ee74c204a7e1f1ba
Suvichaar <suvichaar008@gmail.com>
2024-12-26 20:13:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,8 @@ with tab1:
             r'<link rel="EditURI".*?>',
             r'<link rel="shortlink".*?>',
             r'<link rel="alternate" title="oEmbed.*?>',
-            r'<link rel="alternate" title="JSON" type="application/json.*?>'
+            r'<link rel="alternate" type="application/json\+oembed".*?>',
+            r'<link rel="alternate" type="text/xml\+oembed".*?>'
         ]
         for line_pattern in lines_to_remove:
             html_content = re.sub(line_pattern, '', html_content, flags=re.DOTALL)
